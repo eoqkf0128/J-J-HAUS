bf9057d640d539437d8ddc31d05b0238c637c760
eoqkf0128 <111618167+eoqkf0128@users.noreply.github.com>
2022-12-18 20:18:09 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let observer = new IntersectionObserver((e)=>{
+    e.forEach((박스)=>{
+        if(박스.isIntersecting){
+        박스.target.style.opacity = 1;
+        }
+    })
+})
+
+let section = document.querySelectorAll('section')
+observer.observe(section[0])
+observer.observe(section[1])
+observer.observe(section[2])
+observer.observe(section[3])
